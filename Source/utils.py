c94bb18c259dd017032d49fb701a85c447a21907
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2021-11-29 06:29:54 +0530

    Improved check internet function

--- Source/utils.py
+++ Source/utils.py
@@ -8,7 +8,7 @@ def check_if_git_installed() -> bool:
 
 def check_internet() -> bool:
     try:
-        socket.create_connection(("1.1.1.1", 53))
+        socket.create_connection(("1.2.1.1", 53), timeout=10)
         return True
     except Exception:
         return False
