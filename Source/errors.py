7d24a2adbefb57aa6645f45d4abe0a089024585f
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2021-11-29 06:26:46 +0530

    Refined structure

--- Source/errors.py
+++
@@ -1,8 +0,0 @@
-def no_git_installed():
-    raise RuntimeError("No git installed! Get it here: https://git-scm.com/")
-
-
-def no_internet_connection():
-    raise RuntimeError(
-        "No internet connection! Make sure to have a stable internet connection"
-    )
--- run.py
+++
@@ -1,6 +0,0 @@
-import os
-
-try:
-    os.system("python Source/runtime.py")
-except Exception:
-    os.system("python3 Source/runtime.py")
